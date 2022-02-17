e833dc04927485aac1d78ff48993fbe6d428e735
Marco Túlio Huara <marcotuliohuara@gmail.com>
2022-02-17 04:56:26 -0300

    Adiciona projeto concluído :rocket:

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const list = document.querySelector('#lista-tarefas');
 
-// Risca o item da lista quando o mesmo é clicado duas vezes.
 function crossOutTasks(scratched) {
   const itemScratched = scratched.target;
   if (itemScratched.className === 'completed') {
@@ -10,17 +9,12 @@ function crossOutTasks(scratched) {
   }
 }
 
-// Adiciona o evento de double click nos elementos da lista.
-// const dQallLi = document.querySelectorAll('li');
-
 function doubleClik() {
   for (let i = 0; i < document.querySelectorAll('li').length; i += 1) {
     document.querySelectorAll('li')[i].addEventListener('dblclick', crossOutTasks);
   }
 }
 
-// Pìnta de cinza o intem da lista quando o meso é clicado uma vez.
-// const dGtNli = document.getElementsByTagName('li');
 function colorGrey(item) {
   const itemGrey = item.target;
   for (let i = 0; i < document.getElementsByTagName('li').length; i += 1) {
@@ -29,15 +23,12 @@ function colorGrey(item) {
   itemGrey.style.backgroundColor = 'grey';
 }
 
-
-// Adiciona o evento de click nos itens da lista.
 function eventClick() {
   for (let i = 0; i < document.getElementsByTagName('li').length; i += 1) {
     document.getElementsByTagName('li')[i].addEventListener('click', colorGrey);
   }
 }
 
-// Insere o elemento do input na lista e depois chama o evento1 e evento 2.
 function isertIput() {
   const item = document.getElementById('texto-tarefa').value;
   const itemList = document.createElement('li');
@@ -49,7 +40,6 @@ function isertIput() {
   doubleClik();
 }
 
-// Deleta a lista.
 function deletList() {
   const delet = document.querySelectorAll('li');
   for (let i = 0; i < delet.length; i += 1) {
@@ -57,7 +47,6 @@ function deletList() {
   }
 }
 
-// Remove os itens finalizados que estão riscados.
 function removeFinish() {
   const finish = document.querySelectorAll('.completed');
   for (let i = 0; i < finish.length; i += 1) {
@@ -65,7 +54,6 @@ function removeFinish() {
   }
 }
 
-// Busca a lista salva no localStorage e adiciona o evento1 e evento2.
 function searchEvent() {
   list.innerHTML = localStorage.getItem('list');
   eventClick();
@@ -74,12 +62,10 @@ function searchEvent() {
 
 window.onload = searchEvent;
 
-// Salva a lista no localStorage.
 function saveList() {
   localStorage.setItem('list', list.innerHTML);
 }
 
-// Move o item selecionado para cima na lista.
 function moveUp() {
   for (let i = 0; i < document.getElementsByTagName('li').length; i += 1) {
     if (document.getElementsByTagName('li')[i].style.backgroundColor === 'grey' && (i - 1) !== -1) {
@@ -89,7 +75,6 @@ function moveUp() {
   }
 }
 
-// Move o item selecionado para baixo na lista.
 function moveDown() {
   for (let i = document.getElementsByTagName('li').length - 1; i >= 0; i -= 1) {
     if (document.getElementsByTagName('li')[i].style.backgroundColor === 'grey'
@@ -100,7 +85,6 @@ function moveDown() {
   }
 }
 
-// Remove o item selecionado da lista.
 function removeSe() {
   for (let i = document.getElementsByTagName('li').length - 1; i >= 0; i -= 1) {
     if (document.getElementsByTagName('li')[i].style.backgroundColor === 'grey') {
@@ -108,6 +92,7 @@ function removeSe() {
     }
   }
 }
+
 document.getElementById('criar-tarefa').addEventListener('click', isertIput);
 document.getElementById('funcionamento').addEventListener('dblclick', crossOutTasks);
 document.getElementById('apaga-tudo').addEventListener('click', deletList);
